f56de0e59331117e98487053417c549b2c17df6b
dkafjh <cbksdf2@gmail.com>
2023-10-25 16:44:37 +0900

    5454

--- 키오스크/js/index.js
+++ 키오스크/js/index.js
@@ -1,103 +1,141 @@
-window.onload = function(){
-
+window.onload = function() {
     const selectBtns = document.querySelectorAll('.select-btn > button');
     const modalText = document.querySelector('#howeat'); // 모달 창의 텍스트 요소
     const modal = document.querySelector('#modal');
+    const calculateButton = document.querySelector('#total button');
     
     selectBtns.forEach(function(button) {
-        button.addEventListener('click', function () {
+        button.addEventListener('click', function() {
             const buttonText = button.textContent;
-            console.log(buttonText)
             this.parentNode.classList.add('none');
-            modalText.textContent = buttonText
+            modalText.innerHTML = `<h3> ${buttonText} </h3>`;
         });
     });
     
-    const calculateButton = document.querySelector('#total button');
-    calculateButton.addEventListener('click', function () {
-        if(modalText.textContent === ''){
-            alert('구매를 완료한 후에 눌러주세요!')
+    calculateButton.addEventListener('click', function() {
+        if (modalText.textContent === '') {
+            alert('구매를 완료한 후에 눌러주세요!');
         } else {
             modal.style.display = 'block';
         }
     });
-
+    
     const closeBtn = document.querySelectorAll('.close-btn');
     closeBtn.forEach(button => {
         button.addEventListener('click', function() {
-            if (this.textContent === 'X') {
-                modal.style.display = 'none';
-            } else {
-                this.parentNode.parentNode.style.display = 'none';
-            }
+            modal.style.display = 'none';
         });
     });
     
     const gnbLinks = document.querySelectorAll('.gnb a');
-    gnbLinks.forEach(function (link) {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            // 여기에 메뉴 표시 및 숨김 로직 추가
-            for (const menuId in menuSections) {
-                if (menuSections.hasOwnProperty(menuId)) {
-                    menuSections[menuId].style.visibility = 'hidden';
-                }
-            }
-        
-            // GNB 버튼 클릭 시 해당 메뉴 표시
-            for (const btnId in gnbButtons) {
-                if (gnbButtons.hasOwnProperty(btnId)) {
-                    const button = document.getElementById(btnId);
-                    button.addEventListener('click', event => {
-                        event.preventDefault();
-                        const menuId = gnbButtons[btnId];
-                        for (const id in menuSections) {
-                            if (menuSections.hasOwnProperty(id)) {
-                                menuSections[id].style.visibility = (id === menuId) ? 'visible' : 'hidden';
-                            }
-                        }
-                    });
-                }
-            }
-        });
-    });
-
     const menuSections = {
         'classicMenu': document.getElementById('classicMenu'),
         'specialMenu': document.getElementById('specialMenu'),
         'sideMenu': document.getElementById('sideMenu')
     };
+    
+    gnbLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            // 모든 메뉴 숨김
+            for (const menuId in menuSections) {
+                if (menuSections.hasOwnProperty(menuId)) {
+                    menuSections[menuId].style.display = 'none';
+                }
+            }
 
+            // GNB 버튼 클릭 시 해당 메뉴 표시
+            const menuId = gnbButtons[link.id];
+            menuSections[menuId].style.display = 'block';
+        });
+    });
+    
     const gnbButtons = {
         'classicMenuBtn': 'classicMenu',
         'specialMenuBtn': 'specialMenu',
         'sideMenuBtn': 'sideMenu'
     };
-
-    const $classicMenu = document.querySelectorAll('#classicMenu > div > div');
-    const $specialMenu = document.querySelectorAll('#specialMenu > div > div');
+    
+    const pizzaMenu = document.querySelectorAll('.menu .flex-box div');
     const $addtopping = document.getElementById('addTopping');
+    const sideMenu = document.querySelectorAll('.sidemenu .flex-box div')
 
-
-    function addToppings(){
-        if(!confirm('주문 목록에 담으시겠습니까?')){
-
-        } else {
-            if(confirm('토핑을 추가하시겠습니까?')){
-                modalText.text
-                $addtopping.style.display = 'block'
+    sideMenu.forEach(function(item) {
+        item.addEventListener('click', function() {
+            const menu = item.querySelector('span').textContent;
+            const price = parseFloat(item.getAttribute('data-price'));
+            
+            const addToOrder = confirm(`주문 목록에 ${menu}를 담으시겠습니까?`);
+            if (addToOrder) {
+                const existingInput = document.querySelector(`input[name="${menu}`);
+                const quantityInput = existingInput ? existingInput.nextElementSibling : null;
+    
+                if (quantityInput) {
+                    quantityInput.value = parseInt(quantityInput.value) + 1;
+                } else {
+                    const orderItem = document.createElement('input');
+                    const quantityItem = document.createElement('input');
+    
+                    orderItem.type = quantityItem.type = 'text';
+                    orderItem.name = quantityItem.name = menu;
+                    orderItem.value = `${menu}`;
+                    quantityItem.value = 1;
+                    quantityItem.min = 1;
+    
+                    orderItem.readOnly = quantityItem.readOnly = true;
+    
+                    $orderMenu.appendChild(orderItem);
+                    $orderMenu.appendChild(quantityItem);
+                }
             }
-        }
-    }
+        });
+    });
 
-    $classicMenu.forEach(function(idx){
-        idx.addEventListener('click', addToppings)
-    })
+    pizzaMenu.forEach(function(item) {
+        item.addEventListener('click', function() {
+            const menu = item.querySelector('span').textContent;
+            const price = Number(item.getAttribute('data-price'));
+            
+            const addToOrder = confirm(`주문 목록에 ${menu}를 담으시겠습니까?`);
+            const addToTopping = confirm('토핑을 추가하시겠습니까?')
+            
+            if (addToOrder) {
+                if(addToTopping) {
+                    $addtopping.style.display = 'block'
+                } else{
+                    const existingInput = document.querySelector(`input[name="${menu}`);
+                    const priceInput = document.querySelector(`input[name="${price}`);
+                    const quantityInput = existingInput ? existingInput.nextElementSibling : null;
+                    if (quantityInput) {
+                        quantityInput.value = parseInt(quantityInput.value) + 1;
+                        priceInput.value = parseInt(price) + price;
+                        
+                    } else {
+                        const orderItem = document.createElement('input');
+                        const quantityItem = document.createElement('input');
+                        const pirceItem = document.createElement('input');
+        
+                        orderItem.type = quantityItem.type = pirceItem.type = 'text';
+                        orderItem.name = quantityItem.name = pirceItem.type = menu;
+                        orderItem.value = `${menu}`;
+                        quantityItem.value = 1;
+                        quantityItem.min = 1;
+                        pirceItem.value = price;
+        
+                        orderItem.readOnly = quantityItem.readOnly = true;
+        
+                        $orderMenu.appendChild(orderItem);
+                        $orderMenu.appendChild(quantityItem);
+                    }
+                }
+            } else {
+                return false;
+            }
+        });
+    });
 
-    $specialMenu.forEach(function(idx){
-        idx.addEventListener('click', addToppings)
-    })
+    const $orderMenu = document.getElementById('orderMenu')
+}
 
-    // 초기에 모든 메뉴 숨기기
 
-}    
+
